7d5cb476832ed0daa84b98b763b775941b82660f
Vitor Dutra <dutra.jvitor@gmail.com>
2021-05-29 16:12:10 -0300

    Finish Accordion JS

--- script.js
+++ script.js
@@ -18,3 +18,14 @@ function tabNavigationInit() {
   }
 }
 tabNavigationInit();
+
+const accordionList = document.querySelectorAll('.js-accordion dt');
+
+function activeAccordion() {
+  this.classList.add('ativo');
+  this.nextElementSibling.classList.add('ativo');
+}
+
+accordionList.forEach((item) =>
+  item.addEventListener('click', activeAccordion)
+);
